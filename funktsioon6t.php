070e91de21693a416f8fe001708010ed1386a39a
Jekaterina Vorošilova <jekavor@greeny.cs.tlu.ee>
2015-12-23 17:04:38 +0200

    muudatused

--- funktsioon6t.php
+++ funktsioon6t.php
@@ -9,8 +9,8 @@
 			
 			$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
 			
-			$stmt = $mysqli->prepare("SELECT id, user_id, clothes, brand, size, color FROM fashion");
-			$stmt->bind_result($id, $user_id, $clothes, $brand, $size, $color);
+			$stmt = $mysqli->prepare("SELECT id, user_id, clothes, brand, size, color FROM fashion WHERE deleted IS NULL");
+			$stmt->bind_result($id, $user_id, $clothes, $brand, $size, $color_from_db);
 			$stmt->execute();
 			
 			//tyhi  massiv kus hoiame objekte
@@ -25,7 +25,7 @@
 				$mode->clothes = $clothes;
 				$mode->brand = $brand;
 				$mode->size = $size;
-				$mode->color = $color;
+				$mode->color = $color_from_db;
 				
 				    // lisame selle massiivi
 					array_push($array, $mode);
@@ -44,9 +44,9 @@
 		
 		function deleteMode($id_to_be_deleted){
 		
-		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["dbname"]);
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
 			
-			$stmt = $mysqli->prepare("SELECT user_id, size FROM faschion WHERE id IS NULL");
+			$stmt = $mysqli->prepare("UPDATE fashion SET deleted=NOW() WHERE id=?");
 			$stmt->bind_param("i", $id_to_be_deleted);
 			
 			if($stmt->execute()){
